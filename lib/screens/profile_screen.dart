2b579295d40f168ff621d3922f118e4fea4bcfe7
Jonathan Gilreath <jhgilre@g.clemson.edu>
2025-12-01 23:20:31 -0500

    Add Final Milestone README + Minor UI update

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -41,7 +41,7 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Profile"),
+        title: const Text("Profile",style: TextStyle(fontWeight: FontWeight.bold)),
         backgroundColor: AppColors.bgPrimaryLightBlue,
       ),
       backgroundColor: AppColors.bgPrimaryWhite,
